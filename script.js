d06a03c38633111ad4be9489f775efa8ccd2ce7d
Willow <smurfygirl29@gmail.com>
2025-08-12 16:12:59 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ console.log('This is my test message to make sure that something is working at t
 function randomNumber(num) {
     return Math.floor(Math.random() * num);
 }
+
+const positiveWords = {
+    level1: ['enough', 'wanted', 'needed'],
+    level2: ['capable', 'unstoppable', 'untouchable'],
+    level3: ['loved', 'cherished', 'enjoyed']
+};
+
